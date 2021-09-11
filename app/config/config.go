42edb773bb1f87a4ccd296407637f0b66bbc9424
Stepan Volkov <stepan2volkov@yandex.ru>
2021-09-11 19:56:49 +0500

    getting configuration from env-vars if yaml-file is empty

--- app/config/config.go
+++ app/config/config.go
@@ -3,19 +3,28 @@ package config
 import (
 	"os"
 
+	"github.com/kelseyhightower/envconfig"
 	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
-	Addr              string `yaml:"address"`
-	DSN               string `yaml:"dsn"`
-	ReadTimeout       int    `yaml:"read_timeout"`
-	WriteTimeout      int    `yaml:"write_timeout"`
-	ReadHeaderTimeout int    `yaml:"read_header_timeout"`
+	Addr              string `yaml:"addr"`
+	DSN               string `yaml:"dsn" envconfig:"DSN" default:"memory" required:"true"`
+	ReadTimeout       int    `yaml:"read_timeout" envconfig:"READ_TIMEOUT" default:"30" required:"true"`
+	WriteTimeout      int    `yaml:"write_timeout" envconfig:"WRITE_TIMEOUT" default:"30" required:"true"`
+	ReadHeaderTimeout int    `yaml:"read_header_timeout" envconfig:"READ_HEADER_TIMEOUT" default:"30" required:"true"`
 }
 
 func GetConfig(path string) (Config, error) {
+	if path == "" {
+		return getConfigFromEnv()
+	}
+	return getConfigFromYaml(path)
+}
+
+func getConfigFromYaml(path string) (Config, error) {
 	config := Config{}
+
 	file, err := os.Open(path)
 	if err != nil {
 		return Config{}, err
@@ -25,5 +34,17 @@ func GetConfig(path string) (Config, error) {
 		return Config{}, err
 	}
 
+	return config, nil
+}
+
+func getConfigFromEnv() (Config, error) {
+	config := Config{}
+
+	err := envconfig.Process("", &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	config.Addr = ":" + os.Getenv("PORT")
 	return config, nil
 }
--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/getkin/kin-openapi v0.75.0
 	github.com/go-chi/chi/v5 v5.0.4
 	github.com/jackc/pgx/v4 v4.13.0
+	github.com/kelseyhightower/envconfig v1.4.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
